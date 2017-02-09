4cf596d044d71e55423c09a3069c4966a5341ff2
Charles <Charles@CHARLES-PC>
2017-02-09 15:58:04 +0100

    Ajoutez des fichiers projet.

---
+++ TwitchBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IrcClient irc = new IrcClient("irc.twitch.tv", 6667, "ducmirack", "oauth:qfcb61okru4jyynjjtlz2qvxe2t07e");
+            irc.joinRoom("ducmirack");
+            while (true)
+            {
+                string message = irc.readMessage();
+                if (message.Contains("!hello"))
+                {
+                    irc.sendChatMessage("Yo yo");
+                }
+            }
+        }
+    }
+}
