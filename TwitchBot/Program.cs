985da660da937c60be3639961eab4f418b0cbe83
Etharsan <e.tharsan33@hotmail.fr>
2017-02-23 16:35:34 +0100

    Ajout de systeme de vote

--- TwitchBot/Program.cs
+++ TwitchBot/Program.cs
@@ -9,6 +9,11 @@ namespace TwitchBot
 {
     class Program
     {
+
+        //** Variable pour la partie System de vote
+        private static bool isStartVoting = false;
+        private static Dictionary<string, int> propositions = new Dictionary<string, int>();
+        //****//
         private static IrcClient Irc { get; set; }
 
         static void Main(string[] args)
@@ -25,9 +30,56 @@ namespace TwitchBot
                     string message = Irc.readMessage();
                     if (message != null)
                     {
-                        if (message.Contains("!hello"))
+                        Console.WriteLine(message);
+
+                        if (message.Contains("!vote"))  // Test msg : !vote Quelle jeu aimez-vous ? witcher assassin pacman
                         {
-                            Irc.sendChatMessage("Yo yo");
+                            string propositionAll = message.Substring(message.IndexOf('?') + 2);
+                            if (propositionAll != "")
+                            {
+                                string[] msgPropos = propositionAll.Split(' ');
+                                string sendPropo = "Vote : ";
+                                foreach (string propo in msgPropos)
+                                {
+                                    sendPropo += "!" + propo + " ";
+                                    propositions.Add("!" + propo, 0);
+                                }
+
+                                Irc.sendChatMessage(sendPropo);
+                                isStartVoting = true;
+                            }
+                        }
+
+                        if (isStartVoting)
+                        {
+                            // Terminer le vote
+                            if (message.Contains("!endvote"))
+                            {
+                                isStartVoting = false;
+                                Irc.sendChatMessage("Le vote est terminé !");
+                                string recapMsg = "/me RECAP : ";
+                                string gagnant = "";
+                                int maxVal = 0;
+                                foreach (KeyValuePair<string, int> prop in propositions) {
+                                    if (prop.Value > maxVal) {
+                                        gagnant = "/me LE GAGNANT EST : " + prop.Key;
+                                    }
+                                    recapMsg += prop.Key + " : " + prop.Value.ToString() + ", ";
+                                    maxVal = prop.Value;
+                                }
+                                propositions.Clear();
+                                Irc.sendChatMessage(recapMsg);
+                                Irc.sendChatMessage(gagnant);
+                            }
+
+                            foreach (KeyValuePair<string, int> prop in propositions)
+                            {
+                                if (message.Contains(prop.Key)) {
+                                    propositions[prop.Key] += 1;
+                                    Irc.sendChatMessage(prop.Key + " : " + propositions[prop.Key].ToString());
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
